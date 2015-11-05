e1dcccbba9a5ec43f0c29422720cf59902db73ba
sooheon <tngjs0@gmail.com>
2015-11-06 06:19:26 +0900

    Add Apache Shiro dependency

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject matasano-crypto "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.apache.shiro/shiro-core "1.2.4"]])
