c1ac55788a3ea136f4e5aae313eaac62a03f09c9
sooheon <tngjs0@gmail.com>
2015-11-07 04:38:59 +0900

    Solve challenge 6
    Somehow klutzd it together to pass, my XOR code is in dire need of an
    overhaul.

--- src/matasano_crypto/set_1.clj
+++ src/matasano_crypto/set_1.clj
@@ -2,7 +2,6 @@
   (:import [org.apache.shiro.codec Base64 CodecSupport Hex])
   (:require [clojure.string :as str]))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Challenge 1: hex to base64
 
 (defn hex->base64 [s]
@@ -14,7 +13,6 @@
     (Hex/encodeToString raw)))
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Challenge 2: Fixed XOR
 
 (defn inplace-xor [^bytes a ^bytes b ^bytes out]
@@ -33,18 +31,17 @@
 ;; => "I'm killing your brain like a poisonous mushroom"
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Challenge 3: Single-byte XOR cipher
 
 (def etaoin-shrdlu "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
 
-(defn mask-xor
-  "Given a byte-array and a single-byte mask, returns the xor result."
-  [a mask]
-  (let [a-bytes (Hex/decode a)
-        m-bytes (byte-array (alength a-bytes) mask)
-        out (byte-array (alength a-bytes))]
-    (inplace-xor a-bytes m-bytes out)
+(defn single-byte-xor
+  "Given a string of bytes and a single-byte mask, returns the xor result."
+  [s mask]
+  (let [s-bytes (Hex/decode s)
+        m-bytes (byte-array (alength s-bytes) mask)
+        out (byte-array (alength s-bytes))]
+    (inplace-xor s-bytes m-bytes out)
     (CodecSupport/toString out)))
 
 (defn etaoin-score
@@ -65,11 +62,11 @@
   [s]
   (reduce + (replace {nil -6000} (map etaoin-score s))))
 
-(defn XOR-permute-strings
-  "Takes an encoded string XOR'd against a single character. Returns all
+(defn xor-permute-strings
+  "Takes an string of bytes XOR'd against a single character. Returns all
   permutations of this string XOR'd against a single byte."
   [s]
-  (for [i (range 128)] (mask-xor s (byte i))))
+  (for [i (range 128)] (single-byte-xor s (byte i))))
 
 (defn find-most-english
   "Takes a list of strings and returns the most English of them all."
@@ -77,3 +74,33 @@
   (let [str-score-pairs (into {} (map-indexed (fn [_ x] [x (score-text x)]) v))
         sorted (sort-by val > str-score-pairs)]
     (-> sorted first key)))
+
+
+;; Challenge 4: Detect single-character XOR
+
+(def c4-data
+  (read-string
+   (-> (slurp "resources/4.txt")
+       (str/trim-newline) ;; don't want final \n to be surrounded by \"
+       (str/replace #"\n" "\"\n \"")
+       (str/join ["[\"" "\"]"]))))
+
+#_(find-most-english
+   (map (comp find-most-english xor-permute-strings) c4-data))
+
+;; Challenge 5: Repeating-key XOR
+
+(def ice-ice-lyrics
+  "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal")
+
+(defn repeat-key-xor
+  [a k]
+  (let [a-bytes (CodecSupport/toBytes a)
+        len (alength a-bytes)
+        k-bytes (->> (CodecSupport/toBytes k)
+                     cycle
+                     (take len)
+                     byte-array)
+        out (byte-array len)]
+    (inplace-xor a-bytes k-bytes out)
+    (Hex/encodeToString out)))
--- test/matasano_crypto/set_1_test.clj
+++ test/matasano_crypto/set_1_test.clj
@@ -16,6 +16,18 @@
 (deftest challenge-3
   (let [input "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"]
     (is (= "Cooking MC's like a pound of bacon"
-           (mask-xor input (byte 88))))
+           (single-byte-xor input (byte 88))))
     (is (= "Cooking MC's like a pound of bacon"
-           (find-most-english (XOR-permute-strings input))))))
+           (find-most-english (xor-permute-strings input))))))
+
+(def chal-4-answer
+  (delay (find-most-english
+          (map (comp find-most-english xor-permute-strings) c4-data))))
+
+(deftest challenge-4
+  (is (= "Now that the party is jumping\n"
+         @chal-4-answer)))
+
+(deftest challenge-5
+  (is (= "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+         (repeat-key-xor ice-ice-lyrics "ICE"))))
